a67bfa1e8a4fbb33080f951c5faeab544e1164ad
Nikita Mishkov <mishkov.dd@gmail.com>
2023-09-19 14:26:13 +0300

    Implement first working version

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,11 @@
+// ignore_for_file: avoid_web_libraries_in_flutter
+
+import 'dart:js' as js;
+import 'dart:html' as html;
+
+import 'package:crop_your_image/crop_your_image.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_dropzone/flutter_dropzone.dart';
 
 void main() => runApp(const MyApp());
@@ -17,64 +24,103 @@ class _MyAppState extends State<MyApp> {
   bool highlighted = false;
   EditStage _stage = EditStage.pickup;
 
+  final _cropController = CropController();
+
+  Uint8List? _originImageBytes;
+  Uint8List? _croppedImageBytes;
+
+  var focusNode = FocusNode();
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: const Text('Dropzone example'),
+          title: const Text('Fluid'),
         ),
-        body: _stage == EditStage.pickup
-            ? SizedBox.shrink()
-            : Container(
-                color: highlighted ? Colors.red : Colors.green,
-                margin: const EdgeInsets.all(8.0),
-                child: Stack(
-                  children: [
-                    DropzoneView(
-                      operation: DragOperation.copy,
-                      cursor: CursorType.grab,
-                      onCreated: (ctrl) => controller = ctrl,
-                      onHover: () {
-                        setState(() {
-                          highlighted = true;
-                        });
+        body: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: _stage == EditStage.editing
+              ? _originImageBytes != null
+                  ? RawKeyboardListener(
+                      autofocus: true,
+                      focusNode: focusNode,
+                      onKey: (event) {
+                        if (event.isKeyPressed(LogicalKeyboardKey.enter)) {
+                          _cropController.crop();
+                        }
                       },
-                      onLeave: () {
-                        setState(() {
-                          highlighted = false;
-                        });
-                      },
-                      onDrop: _handleFile,
-                    ),
-                    Center(
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          const Text('Drop something here'),
-                          const Text('or'),
-                          ElevatedButton(
-                            onPressed: () async {
-                              if (controller == null) {
-                                return;
-                              }
-
-                              print(await controller!.pickFiles(
-                                  mime: ['image/jpeg', 'image/png']));
-                            },
-                            child: const Text('Pick file'),
-                          ),
-                        ],
+                      child: Crop(
+                        aspectRatio: 547 / 470,
+                        image: _originImageBytes!,
+                        controller: _cropController,
+                        onCropped: (image) {
+                          setState(() {
+                            _croppedImageBytes = image;
+                            js.context.callMethod("webSaveAs", [
+                              html.Blob([_croppedImageBytes]),
+                              "cropped.png",
+                            ]);
+                          });
+                        },
                       ),
-                    ),
-                  ],
+                    )
+                  : const Center(
+                      child: Text('_imageBytes is null!'),
+                    )
+              : Container(
+                  color: highlighted ? Colors.red : Colors.green,
+                  child: Stack(
+                    children: [
+                      DropzoneView(
+                        operation: DragOperation.copy,
+                        cursor: CursorType.grab,
+                        onCreated: (ctrl) => controller = ctrl,
+                        onHover: () {
+                          setState(() {
+                            highlighted = true;
+                          });
+                        },
+                        onLeave: () {
+                          setState(() {
+                            highlighted = false;
+                          });
+                        },
+                        onDrop: _handleFile,
+                      ),
+                      Center(
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            const Text('Drop something here'),
+                            const Text('or'),
+                            ElevatedButton(
+                              onPressed: () async {
+                                if (controller == null) {
+                                  return;
+                                }
+
+                                final files = await controller!.pickFiles(
+                                    mime: ['image/jpeg', 'image/png']);
+                                await _handleFile(files.firstOrNull);
+                              },
+                              child: const Text('Pick file'),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
                 ),
-              ),
+        ),
       ),
     );
   }
 
-  void _handleFile(file) async {
+  Future<void> _handleFile(file) async {
+    if (file == null) {
+      return;
+    }
     if (controller == null) {
       return;
     }
@@ -82,6 +128,9 @@ class _MyAppState extends State<MyApp> {
     setState(() {
       highlighted = false;
     });
-    final bytes = await controller?.getFileData(file);
+    _originImageBytes = await controller?.getFileData(file);
+    setState(() {
+      _stage = EditStage.editing;
+    });
   }
 }
